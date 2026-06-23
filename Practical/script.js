247d3ab61917cbfc8a8f82da47abb34fa77b1ed6
rajatgangal1710 <rajatgangal1710@gmail.com>
2026-06-23 15:31:21 +0530

    practical

---
+++ Practical/script.js
@@ -0,0 +1,23 @@
+function checkPrime() {
+    let num = parseInt(document.getElementById("num").value);
+    let prime = true;
+
+    if (num <= 1) {
+        prime = false;
+    } else {
+        for (let i = 2; i < num; i++) {
+            if (num % i === 0) {
+                prime = false;
+                break;
+            }
+        }
+    }
+
+    if (prime) {
+        document.getElementById("result").innerHTML =
+            num + " is a Prime Number";
+    } else {
+        document.getElementById("result").innerHTML =
+            num + " is Not a Prime Number";
+    }
+}
